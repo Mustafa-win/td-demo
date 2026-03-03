b8402cd31ddc6c933656ef041db99704a0156fa0
Mustafa Serhat Uslu <mustafa@Mustafas-MacBook-Air.local>
2026-03-03 16:28:11 +0200

    update userAvatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 
+import userAvatar from "../public/userAvatar.png";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
-          src="/userAvatar.png"
+          src={userAvatar}
           alt="User Avatar"
           width={100}
           height={100}
